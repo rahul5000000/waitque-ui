41780a481231ab26fa6f2d18e1a5116f495046cc
Rahul Sinha <rahul@therrsgroup.com>
2026-03-01 18:27:10 -0500

    Add UI support for additional phone numbers

--- src/components/admin/new-customer/NewCustomerQuestionText.tsx
+++ src/components/admin/new-customer/NewCustomerQuestionText.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 
 export default function NewCustomerQuestionText({children, isRequired = false, hasValidationError}) {
   return (
-    <View className="flex-row my-2 mt-5">
+    <View className="flex-row my-2 mt-3">
       {hasValidationError ? <Text className="text-[12px]" style={{color: "red"}}>{children}</Text> : <Text className="text-[12px]">{children}</Text>}
       {isRequired ? <Text style={{color: "red"}}>*</Text> : null}
     </View>
